8dfb09d73482faa4e5ee966d0216aa30f192f02f
GadgetLaboratory <crazygadget466@gmail.com>
2019-12-19 11:56:43 -0500

    "Did what I can"

--- Ball.gd
+++ Ball.gd
@@ -1,25 +1,40 @@
 extends RigidBody2D
 
+var ball = 3
+var speed = 2
+var spawnLoc = get_local_mouse_position()
+var velocity = Vector2()
+
 func _ready():
     contact_monitor = true
     set_max_contacts_reported(4)
     
 func _physics_process(delta):
-    var ball = 3
+    var mouse_x = get_viewport().get_mouse_position().x
+    if get_input() == false:
+        position = Vector2(mouse_x, position.y + 60)
     var bodies = get_colliding_bodies()
-    var spawnLoc = get_local_mouse_position()
-    var damage = 1
-    
+    #var damage = 1
     for body in bodies:
         if body.is_in_group("Tiles"):
-            body.health -= damage
             body.queue_free()
-            
+    
+    # =INTENDED=
+    #for body in bodies:
+        #if body.is_in_group("Tiles"):
+            #body.health -= damage
+            #if body.health == 0:
+                #body.queue_free()
+func get_input():    
+    if Input.is_action_pressed("ui_accept") and speed == 0:
+        velocity += -transform.y * speed
+        velocity += -transform.x * speed
+        
     if position.y > get_viewport_rect().end.y:
         ball -= 1
         if ball == 0:
             queue_free()
-            print("Your chances for escape has run out!")
+            print("Your chances for escape have run out!")
         else:
             position.y = 550
             position.x = spawnLoc
--- Tile.gd
+++ Tile.gd
@@ -3,27 +3,44 @@ extends StaticBody2D
 var tile1 = preload("res://Assets/tile1.png")
 var tile2 = preload("res://Assets/tile2.png")
 var tile3 = preload("res://Assets/tile3.png")
-#var tile4 = preload("res://Assets/tile4.png")
-#var tile5 = preload("res://Assets/tile5.png")
-#var tile6 = preload("res://Assets/tile6.png")
-#var tile7 = preload("res://Assets/tile7.png")
-#var tile8 = preload("res://Assets/tile8.png")
-#var tile9 = preload("res://Assets/tile9.png")
-#var tile10 = preload("res://Assets/tile10.png")
+var tile4 = preload("res://Assets/tile4.png")
+var tile5 = preload("res://Assets/tile5.png")
+var tile6 = preload("res://Assets/tile6.png")
+var tile7 = preload("res://Assets/tile7.png")
+var tile8 = preload("res://Assets/tile8.png")
+var tile9 = preload("res://Assets/tile9.png")
+var tile10 = preload("res://Assets/tile10.png")
 
-var health = int(get_parent().name)
-var score = 0
+# var health = int(get_parent().name) doesn't seem to work correctly, should revise later
 
 onready var sprite = get_node("Sprite")
-func applyTexture():
-    
-    pass
+func applyTexture(txt):
+    sprite.set_texture(txt)
 
 func healthCheck():
-    sprite.set_texture("tile"+(get_parent().name))
-    pass
+    # Wish to revise one day to make the following happen: On collision, check for health and re-apply the matching texture.
+    # sprite.set_texture("tile"+(get_parent().name))
+        # the above doesn't seen to work and quite frankly i'm out of ideas and time
+    pass 
     
-func _ready():
-    sprite.set_texture("tile"+(get_parent().name))
-    
-    
+func _ready():   
+    if get_parent().name == "1":
+       applyTexture(tile1)
+    if get_parent().name == "2":
+       applyTexture(tile2)
+    if get_parent().name == "3":
+       applyTexture(tile3)
+    if get_parent().name == "4":
+       applyTexture(tile4)
+    if get_parent().name == "5":
+       applyTexture(tile5)
+    if get_parent().name == "6":
+       applyTexture(tile6)
+    if get_parent().name == "7":
+       applyTexture(tile7)
+    if get_parent().name == "8":
+       applyTexture(tile8)
+    if get_parent().name == "9":
+       applyTexture(tile9)
+    if get_parent().name == "10":
+       applyTexture(tile10)
